403f95834d96291b65935d19ed6a0ffa7ce20e01
BUG9 <1161650078@qq.com>
2018-06-19 22:51:19 +0800

    zuul网关构建，并实现整合Swagger2，聚合分布式系统文档

--- msc-eureka/src/main/java/com/zhc/msceureka/listener/InstanceCancelListener.java
+++ msc-eureka/src/main/java/com/zhc/msceureka/listener/InstanceCancelListener.java
@@ -1,6 +1,8 @@
 package com.zhc.msceureka.listener;
 
 import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +15,15 @@ import com.zhc.msceureka.email.EmailService;
 @Configuration
 public class InstanceCancelListener implements ApplicationListener<EurekaInstanceCanceledEvent> {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
     private EmailService emailService;
 
     @Override
     public void onApplicationEvent(EurekaInstanceCanceledEvent event) {
+
+        logger.info("服务：" + event.getAppName() + "断开");
+
         emailService.sendMail("服务：" + event.getAppName() + "断开",
                 "服务：" + event.getAppName() + ", IP地址：" + event.getServerId() + "与Eureka断开心跳");
     }
---
+++ msc-gateway/src/test/java/com/zhc/mscgetaway/MscGetawayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhc.mscgetaway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MscGetawayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
